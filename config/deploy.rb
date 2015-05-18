8c538b9ae3d0ae5e19c6b9ab28e19b7c70e8db5e
sfisher <scott.fisher@ucop.edu>
2015-05-18 16:33:41 -0700

    Changing ui-prototype directory to just 'ui' since it will probably be what it's called later.

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ set :repo_url, 'https://github.com/bhavi/stash_prototype.git'
 ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, '/apps/dash2/apps/ui-prototype'
+set :deploy_to, '/apps/dash2/apps/ui'
 
 # Default value for :scm is :git
 set :scm, :git
